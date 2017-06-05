f9aa6c0c6f008184c686783649a8916b89f562c1
courtneypattison <courtney@courtneypattison.com>
2017-06-05 07:44:40 -0400

    refactored with new error messages

--- grt_next_ride.py
+++ grt_next_ride.py
@@ -9,7 +9,7 @@ stop specified on the command line.
 
 import sys
 import time
-import urllib
+import urllib2
 
 import gtfs_realtime_pb2
 
@@ -24,7 +24,7 @@ def get_trip_updates_feed():
     """
     feed = gtfs_realtime_pb2.FeedMessage()
 
-    response = urllib.urlopen(TRIP_UPDATES_URL)
+    response = urllib2.urlopen(TRIP_UPDATES_URL)
     feed.ParseFromString(response.read())
     response.close()
 
@@ -39,14 +39,17 @@ def get_next_ride_times(stop_id):
     next_ride_times = []
     feed = get_trip_updates_feed()
 
+    if not feed.entity:
+        sys.exit("Next ride times are not available from the GRT site.")
+
     for entity in feed.entity:
         if entity.HasField("trip_update"):
             for stop_time_update in entity.trip_update.stop_time_update:
-                if stop_time_update.HasField("stop_id") \
-                        and stop_time_update.stop_id == stop_id \
-                        and stop_time_update.HasField("departure") \
-                        and stop_time_update.departure.HasField("time"):
-                    next_ride_times.append(stop_time_update.departure.time)
+                try:
+                    if stop_time_update.stop_id == stop_id:
+                        next_ride_times.append(stop_time_update.departure.time)
+                except:
+                    pass
 
     return next_ride_times
 
@@ -63,23 +66,22 @@ def display_next_ride_times(next_ride_times):
     Print to console a sorted newline separated list of next ride times, with
     the approximate number of minutes until the next ride
     """
-    next_ride_times.sort()
 
     if next_ride_times:
+        next_ride_times.sort()
+
         mins_to_next_ride = int(next_ride_times[0] - time.time()) / 60
         next_ride_time = format_time(next_ride_times[0])
         print next_ride_time, "in", mins_to_next_ride, "minutes"
-    else:
-        print "Invalid STOP_ID"
-        return
 
-    for next_ride_time in next_ride_times[1:]:
-        print format_time(next_ride_time)
+        for next_ride_time in next_ride_times[1:]:
+            print format_time(next_ride_time)
+    else:
+        print "There are no next ride times."
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print "Usage: " + sys.argv[0] + " STOP_ID"
-        sys.exit(2)
+        sys.exit("Usage:", sys.argv[0], "STOP_ID")
 
     display_next_ride_times(get_next_ride_times(sys.argv[1]))
